1209a55d27bfd5564f3541d33148bc871f031037
Dalton <slattery.da@gmail.com>
2022-02-28 15:37:49 -0600

    commit tree 338619ec679ac6391b540afc39640ea393a5bf4d

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,46 @@
 class Movie < ActiveRecord::Base
+    def self.first_movie
+        Movie.first
+    end
+
+    def self.create_with_title(str)
+        Movie.create(title: str)
+    end
+
+    def self.last_movie
+        Movie.last
+    end
+
+    def self.movie_count
+        Movie.all.size
+    end
+
+    def self.find_movie_with_id(num)
+        Movie.find(num)
+    end
+
+    def self.find_movie_with_attributes(arg)
+        Movie.find_by(arg)
+    end
+
+    def self.find_movies_after_2002
+        Movie.where('release_date > 2002')
+    end
+
+    def update_with_attributes(attributes)
+        self.update(attributes)
+    end
+
+    def self.update_all_titles(str)
+        Movie.update(title: str)
+    end
+
+    def self.delete_by_id(num)
+        Movie.find(num).destroy
+    end
+
+    def self.delete_all_movies
+        Movie.destroy_all
+    end
 
 end
